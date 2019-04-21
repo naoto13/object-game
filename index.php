ca3c502a8864bcaeecce7f07a81ccbddb99d4319
Naoto13 <yamaura777@gmail.com>
2019-04-22 00:36:46 +0900

    practice 8

--- index.php
+++ index.php
@@ -10,38 +10,29 @@ session_start();
 $monsters = array();
 //クラス（設計図）の作成。クラス名の先頭は大文字で
 
-//性別クラス
+//性別クラス（クラス定数）
 class Sex{
-    const MAN = 1;//クラス定数
+    const MAN = 1;
     const WOMAN = 2;
     const OKAMA = 3;
 }
 
-//人クラス（こうすることで後々勇者クラスなどに拡張（継承）できる）
-class Human{
+//抽象クラス（生き物クラス）
+// 各クラスで共通するものを抜き出している。変わらないプロパティは、そのままprotectedで継承先でも使えるように
+//クラスごとに変わるものは抽象メソッドとしてabstract public functionとして定義する
+abstract class Creature{
     protected $name;
-    protected $sex;
     protected $hp;
     protected $attackMin;
     protected $attackMax;
-    public function __construct($name, $sex, $hp, $attackMin, $attackMax){
-        $this->name = $name;
-        $this->sex = $sex;
-        $this->hp = $hp;
-        $this->attackMin = $attackMin;
-        $this->attackMax = $attackMax;
-    }
+    abstract public function sayCry();
+
+    // 共通して使いそうなセッターゲッターを定義
     public function setName($str){
         $this->name = $str;
     }
     public function getName(){
         return $this->name;
-    }
-    public function setSex($num){
-        $this->sex = $num;
-    }
-    public function getSex(){
-        return $this->sex;
     }
     public function setHp($num){
         $this->hp = $num;
@@ -49,8 +40,45 @@ class Human{
     public function getHp(){
         return $this->hp;
     }
+    // attackは以前までは引数なしattack()で人クラスやモンスタークラスの中に書いていたが
+    //   これだと、攻撃する相手がmonster確定なので、混乱して自分を攻撃などができなくなる。
+    //   そのためattackには攻撃する相手を引数としてratgetObjとする（疎結合）
+    //モンスターも人もクリティカルは1/10で発生し、1.5倍の攻撃力と決まっていれば定義しちゃって良い。
+    public function attack($targetObj){
+        $attackPoint = mt_rand($this->attackMin, $this->attackMax);
+        // mt_randを否定（！をつける）することでint型がboolean型に変わるので、if(mt_rand(0,9) == $int)としなくても確率の計算ができる。
+        //0の場合だけfalseとなるのでif文の中に入る１〜９まではtrueなので、if以下には入らない
+        if(!mt_rand(0,9)){//10分の１の確率でモンスターのクリティカル
+            $attackPoint *= 1.5;
+            $attackPoint = (int)$attackPoint;
+            History::set($this->getName().'のクリティカルヒット!!');
+        }
+        $targetObj->setHp($targetObj->getHp()-$attackPoint);
+        History::set($attackPoint.'ポイントのダメージ！');
+    }
+}
+
+//人クラス（こうすることで後々勇者クラスなどに拡張（継承）できる）
+class Human extends Creature{
+    protected $sex;
+    public function __construct($name, $sex, $hp, $attackMin, $attackMax) {
+        $this->name = $name;
+        $this->sex = $sex;
+        $this->hp = $hp;
+        $this->attackMin = $attackMin;
+        $this->attackMax = $attackMax;
+    }
+    
+    public function setSex($num){
+        $this->sex = $num;
+    }
+    public function getSex(){
+        return $this->sex;
+    }
+    
     // クラス定数を用いて、わかりやすくswitch文で分岐させる。
     public function sayCry(){
+        History::set($this->name.'が叫ぶ！');
         switch($this->sex){
             case Sex::MAN :
                 History::set('ぐはぁっ！');
@@ -63,87 +91,44 @@ class Human{
                 break;
         }
     }
-    public function attack(){
-        $attackPoint = mt_rand($this->attackMin, $this->attackMax);
-        if(!mt_rand(0,9)){//10分の１の確率でモンスターのクリティカル
-            $attackPoint *= 1.5;
-            $attackPoint = (int)$attackPoint;
-            History::set($this->getName().'のクリティカルヒット!!');
-        }
-        $_SESSION['monster']->setHp($_SESSION['monster']->getHp()-$attackPoint);
-        History::set($attackPoint.'ポイントのダメージを与えた！');
-    }
 }
 
 //モンスタークラス
-class Monster{
+class Monster extends Creature{
 //プロパティ　継承先でも使用したいのでセレクタはprotectedにする
-    protected $name; // 定義しただけだとnullが入る
-    protected $hp;
     protected $img;
-    protected $attack;
-    // private $attack = ''; // nullを入れたくない場合、空文字などで初期化する
     //コンストラクタも関数（__が目印）
-    public function __construct($name, $hp, $img, $attack) {
+    public function __construct($name, $hp, $img, $attackMin, $attackMax) {
         //this（自分自身のプロパティ）にアクセスしたい
         $this->name = $name;
         $this->hp = $hp;
         $this->img = $img;
-        $this->attack = $attack;
+        $this->attackMin = $attackMin;
+        $this->attackMax = $attackMax;
     }
-    //attackメソッド(これで使いまわせる)
-    public function attack(){
-        $attackPoint = $this->attack;
-        //クリティカルはモンスターが行うので、モンスターの方の攻撃メソッドの中に入れた
-        //10分の１の確率でモンスターのクリティカル
-        // mt_randを否定（！をつける）することでint型がboolean型に変わるので、if(mt_rand(0,9) == $int)としなくても確率の計算ができる。
-        //0の場合だけfalseとなるのでif文の中に入る１〜９まではtrueなので、if以下には入らない
-        if(!mt_rand(0,9)){
-            $attackPoint *= 1.5;
-            $attackPoint = (int)$attackPoint;
-            History::set($this->getName().'のクリティカルヒット!!');
-        }
-        $_SESSION['human']->setHp( $_SESSION['human']->getHp() - $attackPoint );
-        History::set($attackPoint.'ポイントのダメージを受けた！');
-        }
-//セッター2つ（setHP, setAttack）
-    public function setHp($num){
-        //セッターを使うことで、直接代入させずにバリデーションチェックを行ってから代入させることができる
-        // FILTER_VALIDATE_INTでint型かどうかを確認（小数点の排除）
-        $this->hp = filter_var($num, FILTER_VALIDATE_INT);
-        //filter_varは値に対して色々なパターンのバリデーションを行える便利関数
-    }
-    public function setAttack($num){
-        $this->attack = (int)filter_var($num, FILTER_VALIDATE_FLOAT);
-    }
-
-//ゲッター（getName, getHp, getImg, getAttack）
-    public function getName(){
-        return $this->name;
-    }
-    public function getHp(){
-        return $this->hp;
-    }
+    //ゲッター
     public function getImg(){
         // あとあとでimgが入っていなかったら、no-imgをだそう！となった時でも、クラスを書き換えるだけ！
         // もしゲッターメソッドを使っていなければ、取得するコードのかしょ全部を修正しなければいけない！
         // カプセル化をすることで、呼び出す側は「中で何をしているのか」を気にせず、ただ呼び出せばいいだけになる（疎結合）
-        if(empty($this->img)){
-            return 'img/no-img.png';
-        }
+        // if(empty($this->img)){
+        //     return 'img/no-img.png';
+        // }
         return $this->img;
     }
-    public function getAttack(){
-        return $this->attack;
+    public function sayCry(){
+        History::set($this->name.'が叫ぶ！');
+        History::set('はうっ！');
     }
 }
+
 //魔法を使えるモンスタークラス（継承）
 class MagicMonster extends Monster{
     private $magicAttack;
     //継承しているのでこのクラスで宣言していないもの（magicAttack）以外を呼び出せる
-    function __construct($name, $hp, $img, $attack, $magicAttack) {
+    function __construct($name, $hp, $img, $attackMin, $attackMax, $magicAttack) {
         //親クラスのコンストラクタで処理する内容を継承したい場合には親コンストラクタを呼び出す
-        parent::__construct($name, $hp, $img, $attack);
+        parent::__construct($name, $hp, $img, $attackMin, $attackMax);
         $this->magicAttack = $magicAttack;
     }
     public function getMagicAttack(){
@@ -156,22 +141,53 @@ class MagicMonster extends Monster{
     //     $_SESSION['myhp'] -= $this->magicAttack;
     //     $_SESSION['history'] .= $this->magicAttack.'ポイントのダメージを受けた！<br>';
     // }
-
     //Attackメソッドをオーバーライドすることで、「ゲーム進行を管理する処理側」は単にattackメソッドを呼べばいいだけになる
     // 魔法を使えるモンスターは自分で魔法を出すか普通に攻撃するかを判断する
-    public function attack(){
-        $attackPoint = $this->attack;
+    public function attack($targetObj){
         if(!mt_rand(0,4)){ //5分の1の確率で魔法攻撃
             History::set($this->name.'の魔法攻撃!!');
-            $_SESSION['human']->setHp( $_SESSION['human']->getHp() - $this->magicAttack );
+            $targetObj->setHp( $targetObj->getHp() - $this->magicAttack );
             History::set($this->magicAttack.'ポイントのダメージを受けた！');
         }else{
             //通常の攻撃の場合は、親クラスの攻撃メソッドを使うことで
             //親クラスの攻撃メソッドが修正されてもMagicMonsterでも反映される（呼んであげるだけ）
-            parent::attack();
+            parent::attack($targetObj);
         }
     }
 }
+
+// //セッター2つ（setHP, setAttack）
+//     public function setHp($num){
+//         //セッターを使うことで、直接代入させずにバリデーションチェックを行ってから代入させることができる
+//         // FILTER_VALIDATE_INTでint型かどうかを確認（小数点の排除）
+//         $this->hp = filter_var($num, FILTER_VALIDATE_INT);
+//         //filter_varは値に対して色々なパターンのバリデーションを行える便利関数
+//     }
+//     public function setAttack($num){
+//         $this->attack = (int)filter_var($num, FILTER_VALIDATE_FLOAT);
+//     }
+
+// //ゲッター（getName, getHp, getImg, getAttack）
+//     public function getName(){
+//         return $this->name;
+//     }
+//     public function getHp(){
+//         return $this->hp;
+//     }
+//     public function getImg(){
+//         // あとあとでimgが入っていなかったら、no-imgをだそう！となった時でも、クラスを書き換えるだけ！
+//         // もしゲッターメソッドを使っていなければ、取得するコードのかしょ全部を修正しなければいけない！
+//         // カプセル化をすることで、呼び出す側は「中で何をしているのか」を気にせず、ただ呼び出せばいいだけになる（疎結合）
+//         if(empty($this->img)){
+//             return 'img/no-img.png';
+//         }
+//         return $this->img;
+//     }
+//     public function getAttack(){
+//         return $this->attack;
+//     }
+// }
+
 //履歴管理クラス（インスタンス化して複数に増殖させる必要のないクラスなので、静的メンバ（static）にする）
 class History{
     // 引数strを履歴に残す
@@ -194,14 +210,14 @@ class History{
 // $human = new Human('勇者見習い',WOMAN, 500, 40, 120);
 
 $human = new Human('勇者見習い', Sex::MAN, 500, 40, 120);
-$monsters[] = new Monster( 'フランケン', 100, 'img/monster01.png', mt_rand(20, 40) );
-$monsters[] = new MagicMonster( 'フランケンNEO', 300, 'img/monster02.png', mt_rand(20, 60), mt_rand(50, 100) );
-$monsters[] = new Monster( 'ドラキュリー', 200, 'img/monster03.png', mt_rand(30, 50) );
-$monsters[] = new MagicMonster( 'ドラキュラ男爵', 400, 'img/monster04.png', mt_rand(50, 80), mt_rand(60, 120) );
-$monsters[] = new Monster( 'スカルフェイス', 150, 'img/monster05.png', mt_rand(30, 60) );
-$monsters[] = new Monster( '毒ハンド', 100, 'img/monster06.png', mt_rand(10, 30) );
-$monsters[] = new Monster( '泥ハンド', 120, 'img/monster07.png', mt_rand(20, 30) );
-$monsters[] = new Monster( '血のハンド', 180, 'img/monster08.png', mt_rand(30, 50) );
+$monsters[] = new Monster( 'フランケン', 100, 'img/monster01.png', 20, 40 );
+$monsters[] = new MagicMonster( 'フランケンNEO', 300, 'img/monster02.png', 20, 60, mt_rand(50, 100) );
+$monsters[] = new Monster( 'ドラキュリー', 200, 'img/monster03.png', 30, 50 );
+$monsters[] = new MagicMonster( 'ドラキュラ男爵', 400, 'img/monster04.png', 50, 80, mt_rand(60, 120) );
+$monsters[] = new Monster( 'スカルフェイス', 150, 'img/monster05.png', 30, 60 );
+$monsters[] = new Monster( '毒ハンド', 100, 'img/monster06.png', 10, 30 );
+$monsters[] = new Monster( '泥ハンド', 120, 'img/monster07.png', 20, 30 );
+$monsters[] = new Monster( '血のハンド', 180, 'img/monster08.png', 30, 50 );
 
 //インスタンスのなかのプロパティ（属性）にアクセスするときはアロー関数が必要
 // (今までのような連想配列だったら$monster['name']だったが、インスタンスは->だけで良い)
@@ -254,8 +270,10 @@ if(!empty($_POST)){
         if($attackFlg){
 
         // ランダムでモンスターに攻撃を与える
-            History::set('攻撃した！');
-            $_SESSION['human']->attack();
+            History::set($_SESSION['human']->getName().'の攻撃！');
+            // attackメソッドの引数に対象を入れれるようにすることで、自分自身も攻撃対象に設定可能に
+            $_SESSION['human']->attack($_SESSION['monster']);
+            $_SESSION['monster']->sayCry();
             // $attackPoint = mt_rand(50,100);
             //hp = hp - attackpoint を-=として省略
             // セッションの中のmonsterのhpを呼び出す
@@ -265,7 +283,9 @@ if(!empty($_POST)){
             
 
         // モンスターから攻撃を受ける(attackメソッドを使用する)
-            $_SESSION['monster']->attack();
+            History::set($_SESSION['monster']->getName().'の攻撃！');
+            $_SESSION['monster']->attack($_SESSION['human']);
+            $_SESSION['human']->sayCry();
             // if($_SESSION['monster'] instanceof MagicMonster){ //魔法攻撃の行えるモンスターなら
             //     if(!mt_rand(0,4)){ //5分の1の確率で魔法攻撃
             //         $_SESSION['monster']->magicAttack();
@@ -275,9 +295,8 @@ if(!empty($_POST)){
             // }else{ //普通のモンスターなら普通の攻撃
             //  $_SESSION['monster']->attack();
             // }
-
         //人が叫ぶ
-            $_SESSION['human']->sayCry();
+            // $_SESSION['human']->sayCry();
 
         // 自分のhpが0以下になったらゲームオーバー
             if($_SESSION['human']->getHp() <= 0){
